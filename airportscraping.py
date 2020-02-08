3b904131c098f57ab4f9c5f4710ddc153862682c
elkim00 <54359854+elkim00@users.noreply.github.com>
2020-02-08 12:50:34 +0100

    Added XLS feature

--- airportscraping.py
+++ airportscraping.py
@@ -1,12 +1,18 @@
-import requests
 from urllib.request import urlopen
+from openpyxl import Workbook
 import re
 
-html = str(urlopen('http://www.airportcodes.org/#').read()).replace('<br />', '\n')
-list = [i[2:i.index(')')+1] for i in re.findall(".*\([A-Z]{3}\)", str(html))]
-print (list)
-print (len(list))
+html = '\n' + str(urlopen('http://www.airportcodes.org/#').read()).replace('<br />', '\n')
+list = [i[ : i.index(')')+1 ] for i in re.findall(".*\([A-Z]{3}\)", str(html))]
 
-file = open("C:\\Users\\S22178\\Desktop\\airportcodes.txt", "w")
-for i in list: file.write(i + '\n')
-file.close();
+for i in range(0, len(list)):
+    if '\\n' in list[i]:
+        list[i] = list[i][list[i].rfind('\\n')+2:]
+
+wb = Workbook()
+ws = wb.active
+for i in list:
+    nome = i[0:i.index('(')]
+    codice = i[i.index('(')+1:len(i)-1]
+    ws.append([nome, codice])
+wb.save("C:\\Users\\S22178\\Desktop\\airportcodes.xlsx")
